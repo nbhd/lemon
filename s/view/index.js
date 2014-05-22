3e588bbf383f177960e1396c66999f3e852c24ca
oki_nobuhide <oki_nobuhide@cyberagent.co.jp>
2014-05-22 18:21:51 +0900

    commit

--- s/view/index.js
+++ s/view/index.js
@@ -5,10 +5,11 @@ define(function (require, module, exports) {
 
     View.prototype = {
 
+        /**
+         * initialize
+         */
         init: function () {
             console.log('init view');
-
-            var self = this;
 
             this.qrcode = new lemon.core.QRCode($('#qrcode')[0], {
                 width: 300,
@@ -16,32 +17,22 @@ define(function (require, module, exports) {
             });
 
             this.input = $('#url');
-            this.input
-                .on('blur', function () {
-
-                    self.createQR();
-                })
-                .on('keyup', function (e) {
-                    if (e.keyCode !== 13) {
-                        return;
-                    }
-
-                    self.createQR();
-                });
-
-            $('#qrcode').on('click', function () {
-                self.save($('#qrcode img')[0].src, self.input[0].value);
-            })
         },
 
-        createQR: function () {
+        /**
+         * generate qr code
+         */
+        generate: function () {
             var url = this.input[0].value;
 
             this.qrcode.makeCode(url);
         },
 
+        /**
+         * save qr code
+         */
         save: function (url, name){
-
+            console.log('save: ', url, name);
             if (!url) {
                 return;
             }
@@ -50,6 +41,30 @@ define(function (require, module, exports) {
             a.href = url;
             a.setAttribute('download', name || 'noname');
             a.dispatchEvent(new CustomEvent('click'));
+        },
+
+        /**
+         * set events
+         */
+        _setEvents: function () {
+
+            var self = this;
+            this.input
+                .on('blur', function () {
+
+                    self.generate();
+                })
+                .on('keyup', function (e) {
+                    if (e.keyCode !== 13) {
+                        return;
+                    }
+
+                    self.generate();
+                });
+
+            $('.lemon-btn-save').eq(0).on('click', function () {
+                self.save($('#qrcode img')[0].src, self.input[0].value);
+            });
         }
     };
 
