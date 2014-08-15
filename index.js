1bd7e99087f3a28be53170c480af2afd1325ed38
oki_nobuhide <oki_nobuhide@cyberagent.co.jp>
2014-08-15 11:29:31 +0900

    delete no use library

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 requirejs.config({
     "baseUrl": "./s",
     "paths": {
-        "underscore": "../vendor/underscore",
         "jquery": "../vendor/jquery-1.11.1.min",
-        "backbone": "../vendor/backbone",
         "qrcode": "../vendor/qrcode"
     },
     "shim": {
-        "backbone": {
-            "deps": [
-                "underscore",
-                "jquery"
-            ],
-            "exports": "Backbone"
-        },
         "jquery": {
             "exports": "$"
-        },
-        "underscore": {
-            "exports": "_"
         },
         "qrcode": {
             "deps": [
@@ -35,7 +23,7 @@ lemon.core = {};
 /**
  * Entry-point the process
  */
-require(['index/index', 'backbone', 'jquery', 'underscore', 'qrcode'], function (index) {
+require(['index/index', 'jquery', 'qrcode'], function (index) {
 
     lemon.core.QRCode   = QRCode;
 
